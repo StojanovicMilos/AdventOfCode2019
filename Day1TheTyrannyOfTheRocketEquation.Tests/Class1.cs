4148eec8a1cb527087d7b7329cb62da2ca94f942
StojanovicMilos <s.milos@sbb.rs>
2019-12-01 09:10:09 +0100

    Add project files.

---
+++ Day1TheTyrannyOfTheRocketEquation.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using Day1TheTyrannyOfTheRocketEquation;
+
+namespace Day1TheTyrannyOfTheRocketEquation.Tests
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData(12, 2)]
+        [InlineData(14, 2)]
+        [InlineData(1969, 654)]
+        [InlineData(100756, 33583)]
+        public void CalculateFuelRequirement_CalculatesCorrectly(int mass, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.CalculateFuelRequirement(mass));
+        }
+
+    }
+}
---
+++ Day1TheTyrannyOfTheRocketEquation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1TheTyrannyOfTheRocketEquation
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(File.ReadAllLines("Masses.txt").Sum(mass => CalculateFuelRequirement(Convert.ToInt32(mass))));
+        }
+
+        public static int CalculateFuelRequirement(int mass) => mass / 3 - 2;
+    }
+}
